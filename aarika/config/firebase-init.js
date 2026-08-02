fd960ca0fad9740448c99e6f1949d7dff5cd7716
nkrai2004 <nishant.rai@gmail.com>
2026-08-02 08:41:45 +0530

    Update firebase-init.js

--- aarika/config/firebase-init.js
+++ aarika/config/firebase-init.js
@@ -8,21 +8,13 @@ Version: v0.1 Genesis
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 const firebaseConfig = {
-
-    apiKey: "",
-
-    authDomain: "",
-
-    projectId: "",
-
-    storageBucket: "",
-
-    messagingSenderId: "",
-
-    appId: "",
-
-    measurementId: ""
-
+  apiKey: "AIzaSyChmido34vQI3H7kE7dPRiirr7xGySiggw",
+  authDomain: "aarika-production.firebaseapp.com",
+  projectId: "aarika-production",
+  storageBucket: "aarika-production.firebasestorage.app",
+  messagingSenderId: "659399557320",
+  appId: "1:659399557320:web:d72d983f7405aa1cbc25fd",
+  measurementId: "G-60SF582XJS"
 };
 
 const app = initializeApp(firebaseConfig);
