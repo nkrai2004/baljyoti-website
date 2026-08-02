cb8f9e160ce6b5fb30d58312d2fe254cdecb7eb0
nkrai2004 <nishant.rai@gmail.com>
2026-08-02 09:27:51 +0530

    Create student.js

---
+++ aarika/student/student.js
@@ -0,0 +1,28 @@
+import app from "../config/firebase-init.js";
+
+import {
+    getAuth,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "/aarika/";
+
+        return;
+
+    }
+
+    const studentName = document.getElementById("studentName");
+
+    if (studentName) {
+
+        studentName.textContent = `Welcome, ${user.displayName}`;
+
+    }
+
+});
