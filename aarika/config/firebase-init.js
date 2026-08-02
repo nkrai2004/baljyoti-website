adde4fd05fee0cb1bf34cccbd9934d42006b99ff
nkrai2004 <nishant.rai@gmail.com>
2026-08-02 08:29:22 +0530

    Create firebase-init.js

---
+++ aarika/config/firebase-init.js
@@ -0,0 +1,30 @@
+/*
+====================================
+AARIKA Firebase Initializer
+Version: v0.1 Genesis
+====================================
+*/
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+
+    apiKey: "",
+
+    authDomain: "",
+
+    projectId: "",
+
+    storageBucket: "",
+
+    messagingSenderId: "",
+
+    appId: "",
+
+    measurementId: ""
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
