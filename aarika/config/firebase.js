d955d76afce1edede43ca38896fbaf7b4abefc10
nkrai2004 <nishant.rai@gmail.com>
2026-08-02 08:27:28 +0530

    Create firebase.js

---
+++ aarika/config/firebase.js
@@ -0,0 +1,27 @@
+/*
+===============================================
+AARIKA Firebase Configuration
+Version : 0.1 Genesis
+===============================================
+*/
+
+const firebaseConfig = {
+
+    apiKey: "",
+
+    authDomain: "",
+
+    projectId: "",
+
+    storageBucket: "",
+
+    messagingSenderId: "",
+
+    appId: "",
+
+    measurementId: ""
+
+};
+
+// Firebase initialization will be added
+// after the project is created.
