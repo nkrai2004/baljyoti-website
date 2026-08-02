f0f029499b78b47dd2c132207b56059f8fe8961c
nkrai2004 <nishant.rai@gmail.com>
2026-08-02 08:40:26 +0530

    Update firebase.js

--- aarika/config/firebase.js
+++ aarika/config/firebase.js
@@ -6,21 +6,13 @@ Version : 0.1 Genesis
 */
 
 const firebaseConfig = {
-
-    apiKey: "",
-
-    authDomain: "",
-
-    projectId: "",
-
-    storageBucket: "",
-
-    messagingSenderId: "",
-
-    appId: "",
-
-    measurementId: ""
-
+  apiKey: "AIzaSyChmido34vQI3H7kE7dPRiirr7xGySiggw",
+  authDomain: "aarika-production.firebaseapp.com",
+  projectId: "aarika-production",
+  storageBucket: "aarika-production.firebasestorage.app",
+  messagingSenderId: "659399557320",
+  appId: "1:659399557320:web:d72d983f7405aa1cbc25fd",
+  measurementId: "G-60SF582XJS"
 };
 
 // Firebase initialization will be added
