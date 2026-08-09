cbad15e103ec3e34afa87ff5704deb17eae823f4
nkrai2004 <nishant.rai@gmail.com>
2026-08-09 19:14:40 +0530

    Update firebase-config.js

--- aarika/js/firebase-config.js
+++ aarika/js/firebase-config.js
@@ -1,8 +1,9 @@
-// AARIKA Firebase configuration
-// Firebase Project: AARIKA School ERP
 
-const AARIKA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDxtqt-TQZ0kkbo5l5qQ9aBmV36NxqPCtk",
+// AARIKA School ERP
+// Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxtqt-TQZ0kkbo5l5qO9aBmV36NxqPctk",
   authDomain: "aarika-school-erp.firebaseapp.com",
   projectId: "aarika-school-erp",
   storageBucket: "aarika-school-erp.firebasestorage.app",
@@ -10,9 +11,5 @@ const AARIKA_FIREBASE_CONFIG = {
   appId: "1:927822693870:web:2fd0a143d9ef196d6c7aff"
 };
 
-const AARIKA_FIREBASE_ENABLED = true;
-
-export {
-  AARIKA_FIREBASE_CONFIG,
-  AARIKA_FIREBASE_ENABLED
-};
+// Make configuration available globally
+window.firebaseConfig = firebaseConfig;
